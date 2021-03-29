1359f798fcee4a7c38058dbd195e3f25be02d2b5
Jamie Bloem <jamiebloem@hotmail.com>
2021-03-29 17:00:35 +0200

    added useEffect and state

--- src/components/NameItem.js
+++ src/components/NameItem.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {act} from "@testing-library/react";
 
-function NameItem({ name }) {
+function NameItem({ name, activeName, setActiveName }) {
+
+    useEffect(() => {
+        console.log(`${name} is in de DOM geplaatst! `)
+    }, [])
+
+    useEffect(() => {
+        if (name === activeName) {
+        console.log(`${name} wil graag een biertje bestellen! `)}
+    }, [activeName])
+
   return (
     <li>
       <h3>{name}</h3>
-      <button type="button">
+      <button type="button"
+              onClick={() => setActiveName(name)}>
         Geef een rondje!
       </button>
     </li>
